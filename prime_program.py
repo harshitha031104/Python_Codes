d255d3aad2c4373c81d357464e4d344436e7799f
harshitha031104 <harshithamanidandina@gmail.com>
2026-06-06 22:58:44 +0530

    Update prime_program.py

--- prime_program.py
+++ prime_program.py
@@ -7,3 +7,16 @@ if count==2:
   print("Prime Number")
 else:
   print("Not a Prime Number")
+
+
+#prime number sum
+num=int(input("Enter a number:"))
+if num>1:
+    for i in range(2,num):
+        if num%i==0:
+            print("Not Prime")
+            break
+        else:
+            print("prime")
+else:
+    print("not Prime")
