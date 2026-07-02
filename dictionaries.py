c2c531438b7ec40564fec68ec4bdf4b14a1f712e
harshitha031104 <harshithamanidandina@gmail.com>
2026-07-02 23:49:16 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+# 1
+d={"a":11,"b":22,"c":33}
+print(d)
+d1={101:[10,20,30],102:[40,50,60]}
+print(d1)
+print(d1[101])
+
+# 2
+emps={"id":101,"name":"harshi","salary":45000.00,"Address":"Hyderabad"}
+print(emps)
+
+d=eval(input("Enter your dictionaries:"))
+print(d)
+
+emps={"id":101,"name":"harshi","salary":45000.00,"Address":"Hyderabad"}
+print("My id=",emps['id'])
+
+
